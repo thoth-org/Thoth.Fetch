a27327702d368f6bd348d06b70b5a1cba4af588c
Maxime Mangel <maxime.shadaen@protonmail.com>
2019-11-05 16:27:06 +0100

    Use fake and paket as dotnet tools

--- fake.cmd
+++ fake.cmd
@@ -1,11 +1,2 @@
-SET TOOL_PATH=".fake"
-
-IF NOT EXIST "%TOOL_PATH%\fake.exe" (
-  rem #if (version == "latest")
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
-  rem #else
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --version 5.16.1
-  rem #endif
-)
-
-"%TOOL_PATH%/fake.exe" %*
+dotnet tool restore
+dotnet fake %*
--- fake.sh
+++ fake.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
 
-OS=${OS:-"unknown"}
-
-echo $OSTYPE
-if [ "$OS" != "Windows_NT" ]
-then
-  # Allows NETFramework like net45 to be built using dotnet core tooling with mono
-  export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
-fi
-
-set -eu
-set -o pipefail
-
-# liberated from https://stackoverflow.com/a/18443300/433393
-realpath() {
-  OURPWD=$PWD
-  cd "$(dirname "$1")"
-  LINK=$(readlink "$(basename "$1")")
-  while [ "$LINK" ]; do
-    cd "$(dirname "$LINK")"
-    LINK=$(readlink "$(basename "$1")")
-  done
-  REALPATH="$PWD/$(basename "$1")"
-  cd "$OURPWD"
-  echo "$REALPATH"
-}
-
-TOOL_PATH=$(realpath ".fake")
-FAKE="$TOOL_PATH"/fake
-
-if ! [ -e "$FAKE" ]
-then
-  dotnet tool install fake-cli --tool-path "$TOOL_PATH" --version 5.16.1
-fi
-"$FAKE" "$@"
+dotnet tool restore
+dotnet fake $@
